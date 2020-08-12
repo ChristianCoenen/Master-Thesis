b47b5f5e78b1afde54353089e883f4593d8eea32
Christian Coenen <coenen.christian@outlook.com>
2020-08-12 12:39:13 +0200

    weight sharing tests with different network depths now working

--- src/main.py
+++ src/main.py
@@ -8,13 +8,12 @@ print(f"Num GPUs Available: {len(physical_devices)}")
 tf.config.experimental.set_memory_growth(physical_devices[0], True) if physical_devices else None
 
 # Configure and train the Entropy Propagation Network
-epn = EntropyPropagationNetwork(dataset="fashion_mnist")
+epn = EntropyPropagationNetwork()
 epn.train()
 epn.show_reconstructions(epn.x_train_norm)
 epn.show_fake_samples(n_samples=10)
 
 ''' Tests '''
 # verify generator weight sharing after training
-# TODO: it's not working with different network depths at the moment
-test_functions.verify_shared_weights(epn.autoencoder.layers[2], epn.autoencoder.layers[7])
-test_functions.verify_shared_weights(epn.autoencoder.layers[3], epn.autoencoder.layers[6], epn.autoencoder.layers[4])
+if epn.weight_sharing:
+    test_functions.verify_shared_weights(autoencoder=epn.autoencoder, encoder=epn.encoder, decoder=epn.decoder)
--- src/test_functions.py
+++ src/test_functions.py
@@ -1,36 +1,30 @@
 from numpy.testing import assert_array_equal
 import numpy as np
 
-def get_weights_no_bias(layer):
+
+def get_weights_no_bias(layers):
     # Get all weight matrices from a specific layer, ignore the bias arrays
-    weights = []
-    for i, weight_var in enumerate(layer.weights):
-        weights.append(layer.get_weights()[i]) if 'kernel' in weight_var.name else None
+    weights = {}
+    for layer in layers:
+        for i, weight_var in enumerate(layer.weights):
+            if 'kernel' in weight_var.name:
+                weights[weight_var.name] = (layer.get_weights()[i])
     return weights
 
 
-def verify_shared_weights(encoder_layer, decoder_layer, classification_layer=None):
+def verify_shared_weights(autoencoder, encoder, decoder):
     # Get all encoder & decoder weights (no biases, cause they can't be shared anyway)
-    encoder_weights = get_weights_no_bias(encoder_layer)
-    decoder_weights = get_weights_no_bias(decoder_layer)
-    classification_weights = get_weights_no_bias(classification_layer) if classification_layer else None
+    encoder_weights = sorted(get_weights_no_bias(encoder.layers))
+    decoder_weights = sorted(get_weights_no_bias(decoder.layers))
+    autoencoder_weights = sorted(get_weights_no_bias(autoencoder.layers))
 
-    # Verify that the layers only have one weight matrix, otherwise shared weights can't be verified
-    if classification_layer:
-        assert len(encoder_weights) == 1 and len(classification_weights) == 1 and len(decoder_weights) == 2
-    else:
-        assert len(encoder_weights) == 1 and len(decoder_weights) == 1
+    # Verify that all three models have the same number of weights matrices (excluding bias) to verify weight sharing
+    assert len(encoder_weights) == len(decoder_weights) == len(autoencoder_weights)
 
-    # Verify that weights are the same
-    if classification_layer:
-        if decoder_weights[0].shape == encoder_weights[0].shape:
-            assert_array_equal(decoder_weights[0], encoder_weights[0])
-            assert_array_equal(decoder_weights[1], classification_weights[0])
-        else:
-            assert_array_equal(decoder_weights[1], encoder_weights[0])
-            assert_array_equal(decoder_weights[0], classification_weights[0])
-    else:
-        assert_array_equal(decoder_weights[0], encoder_weights[0])
+    # Verify that the autoencoder & decoder are having the same weights as the encoder
+    # Note that the Autoencoder consists of the encoder and decoder model
+    for i in range(len(encoder_weights)):
+        assert_array_equal(decoder_weights[i], encoder_weights[i], autoencoder_weights[i])
 
 
 def get_num_trainable_parameters(autoencoder):
