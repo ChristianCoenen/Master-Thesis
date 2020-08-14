a8e3410e164c34babd9eade335d06a2997b9f973
Christian Coenen <coenen.christian@outlook.com>
2020-08-14 15:03:29 +0200

    removed GPU config, since it's performed automatically

--- src/main.py
+++ src/main.py
@@ -1,10 +1,5 @@
-import tensorflow as tf
 from src.epn import EntropyPropagationNetwork
 
-# Config GPU if available
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-print(f"Num GPUs Available: {len(physical_devices)}")
-tf.config.experimental.set_memory_growth(physical_devices[0], True) if physical_devices else None
 
 # Configure and train the Entropy Propagation Network
 epn = EntropyPropagationNetwork(encoder_dims=[1024, 512, 256], latent_dim=100, weight_sharing=True)
