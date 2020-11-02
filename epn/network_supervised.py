eba45439a5fb5686c203e3a8c329a2bfd24876c8
Christian Coenen <coenen.christian@outlook.com>
2020-11-02 19:05:18 +0100

    WIP

--- epn/network_supervised.py
+++ epn/network_supervised.py
@@ -87,6 +87,8 @@ class EPNetworkSupervised(EPNetwork):
     def generate_latent_and_classification_points(self, n_samples):
         # generate random points in the latent space
         x_latent = np.random.normal(0, 1, size=(n_samples, self.latent_dim))
+        random_samples = np.random.rand(n_samples, *self.encoder.input_shape[1:])
+        x_latent = self.encoder.predict(random_samples)[0]
         x_classification = randint(self.classification_dim, size=n_samples)
         x_classification = to_categorical(x_classification, num_classes=self.classification_dim)
         return x_latent, x_classification
--- main.py
+++ main.py
@@ -10,17 +10,16 @@ epn = EPNetworkSupervised(
     latent_dim=20,
     autoencoder_loss=["categorical_crossentropy", "binary_crossentropy"],
     weight_sharing=True,
-    encoder_dims=[500, 500],
-    discriminator_dims=[500, 500],
+    encoder_dims=[1024, 512, 256, 128],
+    discriminator_dims=[1024, 512, 256],
 )
 # Only run the following line if you have graphviz installed, otherwise make sure to remove it or comment it out
 epn.save_model_architecture_images()
 
 epn.visualize_autoencoder_predictions_to_file(state="pre_autoencoder_training")
-epn.train_autoencoder(epochs=10, batch_size=32, validation_split=0.1)
+epn.train_autoencoder(epochs=3, batch_size=32, validation_split=0.1)
+epn.train(epochs=40, batch_size=128, steps_per_epoch=500, train_encoder=True)
 acc = epn.evaluate()
 epn.visualize_autoencoder_predictions_to_file(state="post_autoencoder_training", acc=acc)
 epn.create_modified_classification_plot(sample_idx=4)
 epn.create_modified_classification_plot(random=True)
-
-epn.train(epochs=40, batch_size=128, steps_per_epoch=500, train_encoder=True)
