5e8e84862a5af3434529d02cf184823d9ed5238c
Christian Coenen <coenen.christian@outlook.com>
2020-11-03 15:09:52 +0100

    Removed the pre autoencoder training due to unimproved results

--- main.py
+++ main.py
@@ -30,12 +30,8 @@ epn = EPNetworkSupervised(
 # Only run the following line if you have graphviz installed, otherwise make sure to remove it or comment it out
 epn.save_model_architecture_images()
 
-epn.visualize_autoencoder_predictions_to_file(state="pre_autoencoder_training")
-epn.train_autoencoder(epochs=3, batch_size=32, validation_split=0.1)
-acc = epn.evaluate()
-epn.visualize_autoencoder_predictions_to_file(state="post_autoencoder_training", acc=acc)
 epn.train(epochs=6, batch_size=32, steps_per_epoch=500, train_encoder=True)
 acc = epn.evaluate()
-epn.visualize_autoencoder_predictions_to_file(state="post_gan_training", acc=acc)
+epn.visualize_autoencoder_predictions_to_file(state="reconstructions_post_gan", acc=acc)
 epn.create_modified_classification_plot(sample_idx=4)
 epn.create_modified_classification_plot(random=True)
