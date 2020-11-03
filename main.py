d1b0f57f77b1e0752071e528e00eb43e1a339e00
Christian Coenen <coenen.christian@outlook.com>
2020-11-03 19:50:00 +0100

    Increased latent space to 50

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ data = datasets.get_mnist(fashion=False)
 # Configure and train the Entropy Propagation Network
 epn = EPNetworkSupervised(
     data=data,
-    latent_dim=20,
+    latent_dim=50,
     autoencoder_loss=["categorical_crossentropy", "binary_crossentropy"],
     weight_sharing=True,
     encoder_dims=[1024, 500, 256],
