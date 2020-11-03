cc13638a4934e3910b670ef5661e25331df90fa3
Christian Coenen <coenen.christian@outlook.com>
2020-11-03 21:09:05 +0100

    Test version

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ data = datasets.get_mnist(fashion=False)
 # Configure and train the Entropy Propagation Network
 epn = EPNetworkSupervised(
     data=data,
-    latent_dim=20,
+    latent_dim=50,
     autoencoder_loss=["categorical_crossentropy", "binary_crossentropy"],
     weight_sharing=True,
     encoder_dims=[1024, 512, 256],
@@ -30,7 +30,7 @@ epn = EPNetworkSupervised(
 # Only run the following line if you have graphviz installed, otherwise make sure to remove it or comment it out
 epn.save_model_architecture_images()
 
-epn.train(epochs=6, batch_size=32, steps_per_epoch=500, train_encoder=True)
+epn.train(epochs=40, batch_size=32, steps_per_epoch=500, train_encoder=True)
 acc = epn.evaluate()
 epn.visualize_autoencoder_predictions_to_file(state="reconstructions_post_gan", acc=acc)
 epn.create_modified_classification_plot(sample_idx=4)
