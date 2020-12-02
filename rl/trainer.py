afd9418a3172ffcc561e8f7bf65ee0a352783dd4
Christian Coenen <coenen.christian@outlook.com>
2020-12-02 12:53:07 +0100

    Replaced random agent with agent described in thesis

--- rl/trainer.py
+++ rl/trainer.py
@@ -1,20 +1,48 @@
 import datetime
 import numpy as np
 import tensorflow as tf
+import random
 from pathlib import Path
 
 
 class Agent:
-    """The world's simplest agent!"""
+    """Simple Q learning agent"""
 
-    def __init__(self, action_space):
+    def __init__(self, observation_space, action_space, discount=0.95):
+        self.observation_space = observation_space
         self.action_space = action_space
-
-    def act(self):
-        return self.action_space.sample()
+        self.discount = discount
+        self.model = self.build_model()
 
     def build_model(self):
-        pass
+        model = tf.keras.Sequential()
+        model.add(
+            tf.keras.layers.Dense(
+                self.action_space.n, input_shape=(self.observation_space,), kernel_initializer="zeros", use_bias=False
+            )
+        )
+        sgd = tf.keras.optimizers.SGD(learning_rate=0.1, momentum=0.0, nesterov=False, name="SGD")
+        model.compile(optimizer=sgd, loss="mse")
+        return model
+
+    def act(self, state):
+        return self.model.predict(state)[0]
+
+    def act_random(self):
+        return self.action_space.sample()
+
+    def get_target_q_values(self, state, action, next_state, reward, done):
+        # Q values for actions not taken should be zero.
+        q_values_state = self.model.predict(state)
+
+        # Q_sa = derived policy = max quality env/action = max_a' Q(s', a')
+        q_values_next_state = np.max(self.model.predict(next_state))
+        if done:
+            q_values_state[0][action] = reward
+        else:
+            # reward + gamma * max_a' Q(s', a')
+            q_values_state[0][action] = reward + self.discount * q_values_next_state
+        return q_values_state
 
 
 class Trainer:
@@ -25,7 +53,7 @@ class Trainer:
         self.env = env
         env.seed(seed)
         self.nr_tiles = np.count_nonzero(env.maze.to_impassable() == 0)
-        self.agent = Agent(self.env.action_space)
+        self.agent = Agent(self.nr_tiles, self.env.action_space)
         # check_env requires stable_baselines3 which requires pytorch - This is a overhead for this single command
         # just comment it in if pytorch and stable_baselines3 is installed
         # check_env(self.env)
@@ -34,7 +62,7 @@ class Trainer:
         # Not required if check_env is commented out. Can be run anyway to have it for the case that check_env is used
         self.env.reset()
 
-    def train(self, epochs, max_episode_length, is_human_mode, randomize_start=True, save_to_file=False):
+    def train(self, epochs, max_episode_length, is_human_mode, epsilon, randomize_start=True, save_to_file=False):
         print(f"Start training. Possible memories: {(self.nr_tiles * self.agent.action_space.n) - 4}")
         memories = []
         start_time = datetime.datetime.now()
@@ -52,7 +80,10 @@ class Trainer:
                 self.env.render("human") if is_human_mode else self.env.render("rgb_array")
 
                 # Get next action
-                action = self.agent.act()
+                if np.random.rand() < epsilon:
+                    action = self.agent.act_random()
+                else:
+                    action = np.argmax(self.agent.act(state))
 
                 # Apply action, get reward and new state
                 next_state, reward, done, _ = self.env.step(action)
@@ -69,6 +100,10 @@ class Trainer:
                 if not any([np.array_equal(s, m[0]) and np.array_equal(action_one_hot, m[1]) for m in memories]):
                     # Reshape from 1, X to X (this is better for storing the data)
                     memories.append([s, action_one_hot, n_s, reward, game_over])
+
+                # Train neural network model
+                target_q_values = self.agent.get_target_q_values(state, action, next_state, reward, done)
+                self.agent.model.fit(state, target_q_values, epochs=1, batch_size=1, verbose=0)
 
                 n_steps += 1
                 state = next_state
