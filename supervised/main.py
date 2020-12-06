be0637bb8d9b10a637b2e4dba283e5a594535db8
Christian Coenen <coenen.christian@outlook.com>
2020-12-06 16:50:54 +0100

    Added pretrain to increase training efficiency

--- supervised/main.py
+++ supervised/main.py
@@ -24,6 +24,7 @@ epn = NetworkSupervised(
 # Only run the following line if you have graphviz installed, otherwise make sure to remove it or comment it out
 epn.save_model_architecture_images()
 
+epn.train_autoencoder(epochs=1, batch_size=32, validation_split=0.1)
 epn.train(epochs=40, batch_size=128, steps_per_epoch=500, train_encoder=True)
 acc = epn.evaluate()
 epn.visualize_autoencoder_predictions_to_file(state="post_gan_training", acc=acc)
